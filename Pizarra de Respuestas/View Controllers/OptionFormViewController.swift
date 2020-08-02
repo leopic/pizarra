c11ee4f3d99d7d659d9857a0a720cc813639e6da
Leo Picado <leopic@gmail.com>
2020-08-02 17:04:59 -0600

    Working better

--- Pizarra de Respuestas/View Controllers/OptionFormViewController.swift
+++ Pizarra de Respuestas/View Controllers/OptionFormViewController.swift
@@ -6,13 +6,13 @@ class OptionFormViewController: UIViewController {
   @IBOutlet weak var textField: UITextField!
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var deleteButton: UIButton!
-  @IBOutlet weak var deleteButtonTopConstraint: NSLayoutConstraint!
+  @IBOutlet weak var collectionViewBottomConstraint: NSLayoutConstraint!
 
   @IBAction func didTapOnSave(_ sender: Any) {
     guard let text = textField.text,
       text != "" else { return }
 
-    let option = Option(label: text, backgroundColor: nil)
+    let option = Option(label: text, backgroundColor: selectedColor)
 
     if let index = self.index {
       screen.options[index] = option
@@ -31,8 +31,13 @@ class OptionFormViewController: UIViewController {
 
   var screen: AScreen!
   var index: Int?
+  var selectedColor: UIColor? {
+    didSet {
+      collectionView.reloadData()
+    }
+  }
 
-  private let colorList: [UIColor] = [
+  static let colorList: [UIColor] = [
     .systemBlue,
     .systemGreen,
     ColorCompatibility.systemIndigo,
@@ -47,19 +52,71 @@ class OptionFormViewController: UIViewController {
     .clear
   ]
 
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    collectionView.delegate = self
+    collectionView.dataSource = self
+  }
+
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
 
     navigationItem.title = index == nil ? "Crear" : "Editar"
 
     guard let index = self.index else {
-      deleteButtonTopConstraint.isActive = false
+      collectionViewBottomConstraint.isActive = false
       deleteButton.isHidden = true
       return
     }
 
     let option = screen.options[index]
     textField.text = option.label
+    selectedColor = option.backgroundColor
   }
 }
 
+extension OptionFormViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    Self.colorList.count
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath)
+
+    let color = Self.colorList[indexPath.row]
+    cell.contentView.backgroundColor = color == .clear ? color : color.withAlphaComponent(0.5)
+    cell.contentView.layer.borderColor = Color.label.withAlphaComponent(0.25).cgColor
+    cell.contentView.layer.borderWidth = 1
+    cell.contentView.layer.cornerRadius = 8
+
+    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+    label.textAlignment = .center
+    label.accessibilityIdentifier = "Selected"
+
+    cell.contentView.subviews.first(where: { $0 is UILabel })?.removeFromSuperview()
+
+    if selectedColor == color {
+      cell.contentView.layer.borderWidth = 3
+      label.text = "X"
+      label.shadowColor = ColorCompatibility.systemBackground.withAlphaComponent(0.25)
+      label.shadowOffset = CGSize(width: 1, height: 1)
+      label.font = UIFont(name: "Helvetica-Neue Thin", size: 24.0)
+      cell.contentView.addSubview(label)
+    }
+
+    return cell
+  }
+}
+
+extension OptionFormViewController: UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    selectedColor = Self.colorList[indexPath.row]
+    collectionView.reloadData()
+  }
+}
+
+extension OptionFormViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    return CGSize(width: 64.0, height: 64.0)
+  }
+}
