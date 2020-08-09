3280f32a8df677e7e1b39c7df5f55216165ae40b
Leo Picado <leopic@gmail.com>
2020-08-08 18:56:51 -0600

    Introduce basic logger

--- Pizarra de Respuestas/Models/Logger.swift
+++ Pizarra de Respuestas/Models/Logger.swift
@@ -2,16 +2,47 @@ import Foundation
 
 final class Logger: TextOutputStream {
   static var track = Logger()
-  private static let formatter = DateFormatter()
+  var fileManager = FileManager.default
+
+  private var fileURL: URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    let baseDirectory = paths[0]
+    let formatter = DateFormatter()
+    formatter.dateFormat = "dd-MM-yyyy"
+    let dateString = formatter.string(from: Date())
+    let fileName = "\(dateString).log"
+
+    return baseDirectory.appendingPathComponent(fileName)
+  }
 
   private init() {
-    Self.formatter.dateFormat = "MM/dd/yyyy HH:mm:ss"
+    print("logfile: \(fileURL.absoluteString)")
   }
 
   func write(_ string: String) {
-    let prefix = "___"
-    let date = Self.formatter.string(from: Date())
-    print("\(prefix) \(date) \(string)")
+    guard let messageAsData = formatMessage(string) else {
+      print("Logger.ERROR: Unable to turn message: \(string) into data")
+      return
+    }
+
+    guard fileManager.fileExists(atPath: fileURL.path),
+        let fileHandle = try? FileHandle(forWritingTo: fileURL) else {
+      print("Logger.ERROR: File does not exist or unable to get a filehandle")
+
+      do {
+        try messageAsData.write(to: fileURL, options: .atomicWrite)
+      } catch {
+        print("Logger.ERROR: Unable to write to file")
+      }
+
+      return
+    }
+
+    fileHandle.seekToEndOfFile()
+    fileHandle.write(messageAsData)
+    fileHandle.closeFile()
+
+    print(string)
   }
 
   func screen(_ string: String) {
@@ -20,5 +51,13 @@ final class Logger: TextOutputStream {
 
   func action(_ string: String) {
     write(string)
+  }
+
+  private func formatMessage(_ string: String) -> Data? {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm:ss"
+    let timestamp = formatter.string(from: Date())
+
+    return "\(timestamp): \(string)\n".data(using: .utf8)
   }
 }
