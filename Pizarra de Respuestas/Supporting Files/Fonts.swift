e0f6152fdfc46d7bed65164cb2fc1e6fbf83e528
Leo Picado <leopic@gmail.com>
2020-10-12 21:54:52 -0600

    Tidy up fonts

--- Pizarra de Respuestas/Supporting Files/Fonts.swift
+++ Pizarra de Respuestas/Supporting Files/Fonts.swift
@@ -1,33 +1,6 @@
 import Foundation
 import UIKit
 
-enum Fontarello {
-  case largeTitle
-
-  var name: String {
-    let base = "AvenirNextCondensed"
-
-    switch self {
-    case .largeTitle:
-      return "\(base)-Bold"
-    }
-  }
-
-  var size: CGFloat {
-    switch self {
-    case .largeTitle:
-      return 34
-    }
-  }
-
-  var style: UIFont.TextStyle {
-    switch self {
-    case .largeTitle:
-      return .largeTitle
-    }
-  }
-}
-
 struct Fonts {
   // Additional sizes over at https://gist.github.com/zacwest/916d31da5d03405809c4
   enum Size: CGFloat {
@@ -35,10 +8,9 @@ struct Fonts {
     case title1 = 28
     case title2 = 22
     case title3 = 20
-    case headline = 17
+    case headline = 17 // body
     case callout = 16
     case subheadline = 15
-//    case body = 17 - fak
     case footnote = 13
     case caption1 = 12
     case caption2 = 11
@@ -48,36 +20,38 @@ struct Fonts {
   private static let normalFont = "AvenirNextCondensed-Medium"
 
   static var largeTitle: UIFont {
+    let font = UIFont(name: strongFont, size: .largeTitle)
     let fontMetrics = UIFontMetrics(forTextStyle: .largeTitle)
-    let font = UIFont(fontarello: .largeTitle)!
     return fontMetrics.scaledFont(for: font)
   }
 
   static var title1: UIFont {
-    let font = UIFont(name: strongFont, size: .title1)!
+    let font = UIFont(name: strongFont, size: .title1)
     let fontMetrics = UIFontMetrics(forTextStyle: .title1)
     return fontMetrics.scaledFont(for: font)
   }
 
   static var title2: UIFont {
-    let font = UIFont(name: normalFont, size: .title1)!
+    let font = UIFont(name: normalFont, size: .title1)
     let fontMetrics = UIFontMetrics(forTextStyle: .title1)
     return fontMetrics.scaledFont(for: font)
   }
 
   static var title3: UIFont {
-    let font = UIFont(name: normalFont, size: .title3)!
+    let font = UIFont(name: normalFont, size: .title3)
     let fontMetrics = UIFontMetrics(forTextStyle: .title3)
+    return fontMetrics.scaledFont(for: font)
+  }
+
+  static var headline: UIFont {
+    let font = UIFont(name: normalFont, size: .headline)
+    let fontMetrics = UIFontMetrics(forTextStyle: .headline)
     return fontMetrics.scaledFont(for: font)
   }
 }
 
-extension UIFont {
-  convenience init?(name fontName: String, size fontSize: Fonts.Size) {
-    self.init(name: fontName, size: fontSize.rawValue)
-  }
-
-  convenience init?(fontarello: Fontarello) {
-    self.init(name: fontarello.name, size: fontarello.size)
+private extension UIFont {
+  convenience init(name fontName: String, size fontSize: Fonts.Size) {
+    self.init(name: fontName, size: fontSize.rawValue)!
   }
 }
